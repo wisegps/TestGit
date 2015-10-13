eba6111dd6f42c45624b59bcfbf03c552612949e
c <c@c-PC>
2015-10-13 14:23:03 +0800

    11

--- src/com/example/testg/MainActivity.java
+++ src/com/example/testg/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 
 
@@ -19,7 +20,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        
+        System.out.println("222");
         return true;
     }
 
