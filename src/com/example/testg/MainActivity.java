578b4f9ced8e6347a15f0cb4e6796f1b5c8e4107
c <c@c-PC>
2015-10-13 13:45:42 +0800

    asdf

--- src/com/example/testg/MainActivity.java
+++ src/com/example/testg/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        System.out.println("1111");
         return true;
     }
 
