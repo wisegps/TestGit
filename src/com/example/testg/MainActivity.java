80cf2d93838ff1421ba028236d8cd011cf4952d1
c <c@c-PC>
2015-11-09 15:27:14 +0800

    222

--- src/com/example/testg/MainActivity.java
+++ src/com/example/testg/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        System.out.println("222");
         return true;
     }
 
